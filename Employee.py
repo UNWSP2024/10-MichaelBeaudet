bc738e6ab050f4239ea3f9a8b6c6eddd2fa98ce1
MichaelBeaudet <michaeltbeaudetalt@gmail.com>
2025-04-01 21:23:58 -0500

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -1,10 +1,32 @@
-# Program #4 Employee Class:
-# Write a class Employee that holds the following data about an employee in attributes: name, ID number, department, and job title.
+# Author: Michael Beaudet
+# Title: Program 4 Week 10: Employee Class
+# Date: 4/1/25
 
-# Once you have written the class, write a program that creates three Employee objects to hold the following data.
+class Employee:
+# Initialize each variable
+    def __init__(self, name, id_number, department, job_title):
+        self.name = name
+        self.id_number = id_number
+        self.department = department
+        self.job_title = job_title
+# Display each piece of data
+    def employee_info(self):
+        print(f"Name: {self.name}")
+        print(f"ID Number: {self.id_number}")
+        print(f"Department: {self.id_number}")
+        print(f"Job Title: {self.job_title}")
+        print('-' * 30)
 
-# Name	ID Number	Department	Job Title
-# Susan Meyers	47899 	Accounting	Vice President
-# Mark Jones	39119	IT	Programmer
-# Joy Rogers	81774	Manufacturing	Engineer
-# The program should store the data in the three objects, then display the data for each employee on the screen.
+def main():
+# Create each employee object
+    employee1 = Employee("Susan Meyers", 47899, "Accounting", "Vice President")
+    employee2 = Employee("Mark Jones", 39119, "IT", "Programmer")
+    employee3 = Employee("Joy Rogers", 81774, "Manufacturing", "Engineer")
+# Display each object
+    employee1.employee_info()
+    employee2.employee_info()
+    employee3.employee_info()
+            
+if __name__ == "__main__":
+    main()
+
