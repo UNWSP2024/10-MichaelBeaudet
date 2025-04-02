6a505403473cfc3c0a1e0c464eed36b718d1d624
MichaelBeaudet <michaeltbeaudetalt@gmail.com>
2025-04-01 21:09:19 -0500

    Update Car.py

--- Car.py
+++ Car.py
@@ -1,14 +1,39 @@
-# Program # 2: Car Class
-# Write a class named Car that has the following data attributes:
+# Author: Michael Beaudet
+# Title: Program 2 Week 10: Car Class
+# Date: 4/1/25
 
-# __year_model (for the car's year model)
-# __make (for the make of the car)
-# __speed (for the car's current speed)
-# The Car class should have an __init__ method that accepts the car's year model and make as arguments.  These values should be assigned to the object's __year_model and __make data attributes.  It should also assign 0 to the __speed data attribute.
+class Car:
+    
+# Initialize each variable
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+# Increase the speed by 5
+    def accelerate(self):
+        self.__speed += 5
+# Decrease the speed by 5  
+    def brake(self):
+        if self.__speed >= 5:
+            self.__speed -= 5
+        else:
+            self.__speed = 0
+# Return the current speed of the car
+    def get_speed(self):
+        return self.__speed
+    
+# Create the Car object program
+my_car = Car(2023, "Bugatti Chiron")
+# Accelerate 5 times
+print('Accelerating:')
+for _ in range(5):
+    my_car.accelerate()
+    print('Speed', my_car.get_speed())
+# Brake 5 times
+print('\nBraking:')
+for _ in range(5):
+    my_car.brake()
+    print('Speed:', my_car.get_speed())
 
-# The class should also have the following methods:
 
-# The accelerate method should add 5 to the speed data attribute each time it it called.
-# The brake method should subtract 5 from the speed data attribute each time it is called.
-# The get_speed method should return the current speed.
-# Next, design a program that creates a Car object then calls the accelerate method five times.  After each call to the accelerate method, get the current speed of the car and display it.  The call the brake method.  After each call to the brake method, get the current speed of the car and display it.
+
